719933d235b7a567111cc1f8e7f90f6c64895734
Colin Campbell <colinc86@gmail.com>
2022-04-24 01:56:45 -0500

    Update tests.

--- Tests/DefaultsTests/DefaultsTests.swift
+++ Tests/DefaultsTests/DefaultsTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import Defaults
 
 final class DefaultsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Defaults().text, "Hello, World!")
-    }
+
 }
